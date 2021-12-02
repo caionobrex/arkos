3199e41d76eeff45ed32946d39f7d843b728e9c5
Caio Nobre <marialoucos@hotmail.com.br>
2021-12-02 20:02:38 -0300

    padding ajustado para responsividade em tablets

--- components/container.js
+++ components/container.js
@@ -1,3 +1,3 @@
 export default function Container ({ children }) {
-  return <div className="px-6 md:px-32 2xl:px-96">{children}</div>
+  return <div className="px-6 md:px-16 lg:px-32 2xl:px-96">{children}</div>
 }
